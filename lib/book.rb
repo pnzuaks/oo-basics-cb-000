b2fde683337c722508b38264a72d696739d15847
Patricia Nykaza <pnykaza@calstatela.edu>
2018-05-30 19:04:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
   
-  def initialize(title)
-    @title = title
-  end
+  attr_reader :title
   
-  def title
-    @title
-  end
+  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
